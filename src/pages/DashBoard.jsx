a977b566314493d879c0c12d8ae9e5cef7292eb5
IALikhon <likhon9112@gmail.com>
2026-07-22 22:22:52 +0600

    refactor: restructure DashBoard component layout for improved styling

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -5,15 +5,17 @@ import data from "../data/data";
 
 const DashBoard = () => {
   return (
-    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-8 ">
-      <div className="bg-white rounded-lg shadow p-4">
-        <AreaChartComponent d={data} />
-      </div>
-      <div className="bg-white rounded-lg shadow p-4">
-        <LineChartComponent d={data} />
-      </div>
-      <div className="bg-white rounded-lg shadow p-4">
-        <BarChartComponent d={data} />
+    <div>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-8 ">
+        <div className="bg-white rounded-lg shadow p-4 dark:bg-gray-800 dark:text-white">
+          <AreaChartComponent d={data} />
+        </div>
+        <div className="bg-white rounded-lg shadow p-4 dark:bg-gray-800 dark:text-white">
+          <LineChartComponent d={data} />
+        </div>
+        <div className="bg-white rounded-lg shadow p-4 dark:bg-gray-800 dark:text-white">
+          <BarChartComponent d={data} />
+        </div>
       </div>
     </div>
   );
